b68ace78f3e4c21cd87bd5aad038790e4ff1a305
darder44 <60207231+darder44@users.noreply.github.com>
2020-01-25 16:46:26 +0800

    refactor

--- index.php
+++ index.php
@@ -1,49 +1,7 @@
-<?php
-session_start();
-     if (isset($_GET["login"])){
-          class MyDB extends SQLite3
-   {
-      function __construct()
-      {
-         $this->open('database.db');
-      }
-   }
-   $db = new MyDB();
-   if(!$db){
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
-   }
-
-   $sql ='SELECT * from USERS where USERNAME="'.$_POST["usr_name"].'";';
-
-
-   $ret = $db->query($sql);
-   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-      $id=$row['ID'];
-      $username=$row["USERNAME"];
-      $password=$row['PASSWORD'];
-  }
-    if ($id!=""){
-        if ($password==$_POST["pwd"]){
-           $_SESSION["login"]=$username;
-           header('Location: index.php');    
-        }else{
-          
-          echo "Wrong Password";
-        }
-      }else{
-       echo "User not exist, please register to continue!";
-      }
-   //echo "Operation done successfully\n";
-   $db->close();
-     }
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Log in</title>
+  <title>Log In/Sign Up Form Example in PHP and SQLITE Database</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -51,24 +9,10 @@ session_start();
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <div class="container">
-  <h2>Vertical (basic) form</h2>
-  <form role="form" action="login.php?login=true">
-    <div class="form-group">
-      <label for="usr_name">Username:</label>
-      <input type="text" class="form-control" id="usr_name" placeholder="Enter Username">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox"> Remember me</label>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
+  <h2>Log In/Sign Up Form Example in PHP and SQLITE Database</h2>
+  <button type="button" href="register.php" class="btn btn-primary">Sign Up</button>
+  <button type="button" href="login.php" class="btn btn-primary active">Log in</button>
 </div>
-
 </body>
 </html>
