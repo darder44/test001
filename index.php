cdcd144868c3ff12cc3a9d606b6b6ae2319137fd
darder44 <60207231+darder44@users.noreply.github.com>
2020-01-25 18:04:22 +0800

    refactor

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '1qazzaq1');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
@@ -11,6 +20,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    debug_to_console("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
