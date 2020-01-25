bf031b8e45204cb93d158c06d84dd5b97b0ef4a5
darder44 <60207231+darder44@users.noreply.github.com>
2020-01-25 18:09:04 +0800

    refactor

--- index.php
+++ index.php
@@ -20,6 +20,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    debug_to_console("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
