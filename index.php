81d667c22ed79b898473ffcfedb797742f4cf8a6
darder44 <60207231+darder44@users.noreply.github.com>
2020-01-25 17:56:01 +0800

    refactor

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Log In/Sign Up Form Example in PHP and SQLITE Database</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-  <h2>Log In/Sign Up Form Example in PHP and SQLITE Database</h2>
-  <button type="button" href="register.php" class="btn btn-primary">Sign Up</button>
-  <button type="button" href="login.php" class="btn btn-primary active">Log in</button>
-</div>
-</body>
-</html>
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
