6e48cd04f73580e2c6d8a899d99e46ac39f0b3de
3der <1347729458@qq.com>
2020-02-04 10:49:46 +0800

    update unit test files

--- test/xxxxxTest.php
+++ test/xxxxxTest.php
@@ -5,8 +5,11 @@ class HelloTest extends TestCase
 {
     function testHello()
     {
-        /** 這是斷言，就像是 IF 中的等於，左邊必須等於右邊的值，否則就讓 PHPUnit 報錯 */
         $this->assertEquals('bar', 'bar');
+    }
+    function testHello_Failed()
+    {
+        $this->assertEquals('bar', 'bars');
     }
 }
 ?>
